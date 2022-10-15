0bcc275e51975209817c13bb2f16dd064c83069f
Emiliano López <emilianolch@gmail.com>
2022-10-14 21:23:27 -0300

    show notification

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -14,8 +14,32 @@ class NotificationsController < ApplicationController
   end
 
   def send_message
+    message = {
+      title: 'Hello push',
+      body: 'Hello world!'
+    }
+
     respond_to do |format|
-      format.html { head :no_content }
+      format.turbo_stream do
+        subscriber = PushSubscription.find_by(endpoint: params[:endpoint])
+        Webpush.payload_send(
+          message: message.to_json,
+          endpoint: subscriber.endpoint,
+          p256dh: subscriber.p256dh,
+          auth: subscriber.auth,
+          vapid: {
+            subject: 'mailto:sender@example.com',
+            public_key: ENV['VAPID_PUBLIC_KEY'],
+            private_key: ENV['VAPID_PRIVATE_KEY']
+          },
+          ssl_timeout: 5, # value for Net::HTTP#ssl_timeout=, optional
+          open_timeout: 5, # value for Net::HTTP#open_timeout=, optional
+          read_timeout: 5 # value for Net::HTTP#read_timeout=, optional
+        )
+        render turbo_stream: turbo_stream.append('messages', "Message sent at #{Time.now}\n")
+      rescue StandardError => e
+        render turbo_stream: turbo_stream.append('messages', "#{e.message}\n")
+      end
     end
   end
 end
---
+++ public/service-worker.js
@@ -0,0 +1,8 @@
+self.addEventListener("push", function (event) {
+  const message = event.data.json();
+  event.waitUntil(
+    self.registration.showNotification(message.title, {
+      body: message.body,
+    })
+  );
+});
